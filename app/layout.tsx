88f26f1504cc37092147e6fb06185954e158cdb0
thamsanqaJ <thamsanqa.dev@gmail.com>
2023-09-25 18:56:11 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+// Global variables
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+
+// Local Variables
+import './globals.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ecommerce Admin',
+  description: 'Ecommerce Admin Dashboard',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+       <ClerkProvider>
+    <html lang="en">
+
+      <body className={inter.className}>{children}</body>
+    </html>
+       </ClerkProvider>
+  )
+}
