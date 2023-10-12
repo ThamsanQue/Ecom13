abbeb758bf55a9e67b76671ef05f6fb4e3c7f37b
thamsanqaj <thamsanqa.dev@gmail.com>
2023-10-12 21:48:43 +0200

    env setup, db setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,34 @@
-// Global variables
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// Global imports
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-// Local Variables
-import './globals.css'
+// Local imports
+import "./globals.css";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Ecommerce Admin',
-  description: 'Ecommerce Admin Dashboard',
-}
+  title: "Ecommerce Admin",
+  description: "Ecommerce Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-       <ClerkProvider>
-    <html lang="en">
-
-      <body className={inter.className}>{children}</body>
-    </html>
-       </ClerkProvider>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
